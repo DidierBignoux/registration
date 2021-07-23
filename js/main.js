ec00789e4e26239df91ee7bc5a5ed1e911486664
Bignoux Didier <didieransleybignoux@live.com>
2021-07-23 10:39:55 +0400

    Change Info

--- js/main.js
+++ js/main.js
@@ -4,13 +4,17 @@ var $firstButton = $(".first"),
   $name = $("#name"),
   $phone = $("#phone"),
   $age = $("#age"),
+  $team = $("#team"),
   $more = $(".more"),
   $yourname = $(".yourname"),
+  $yourphone = $(".yourphone"),
   $modifyBtn = $("#modify-btn");
 ($reset = $(".reset")), ($ctr = $(".container"));
 
 var $nameValidation = $("#name-valid"),
-  $phoneValidation = $("#phone-valid");
+  $phoneValidation = $("#phone-valid"),
+  $ageValidation = $("#age-valid"),
+  $teamValidation = $("#team-valid");
 
 $firstButton.on("click", function (e) {
   if ($name.val().length >= 1 && $phone.val().length >= 1 && $phone.val().length <= 8) {
@@ -37,18 +41,30 @@ $secondButton.on("click", function (e) {
       $ctr.addClass("full slider-three-active").removeClass("center slider-two-active slider-one-active");
       $name = $name.val();
       $phone = $phone.val();
+      $age = $age.val();
+      $team = $team.val();
       $nameValidation.val($name);
       $phoneValidation.val($phone);
+      $ageValidation.val($age);
+      $teamValidation.val($team);
+
+      $yourname.html($name + " !");
+      $yourphone.html($phone + " !");
+
       if ($name == "") {
         $yourname.html("Anonymous!");
       } else {
-        $yourname.html($name + "!");
       }
     });
   e.preventDefault();
 });
 
 $modifyBtn.on("click", function (e) {
-  $ctr.addClass("slider-one-active").removeClass("full slider-three-active");
+  $(this)
+    .text("Canceling...")
+    .delay(900)
+    .queue(function () {
+      location.reload();
+    });
   e.preventDefault();
 });
